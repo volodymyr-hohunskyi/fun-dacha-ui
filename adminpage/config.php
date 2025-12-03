feaded75051d94fc8e3330d1dcc0eec24e346b95
Volodymyr Gogunsky <vgogunsky@apple.com>
2025-12-03 12:42:31 +0200

    Added base implementation

---
+++ adminpage/config.php
@@ -0,0 +1,40 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'https://fun-dacha.com.ua/adminpage/');
+define('HTTP_CATALOG', 'https://fun-dacha.com.ua/');
+
+// DIR
+define('DIR_OPENCART', '/home/xjgqivxn/public_html/');
+define('DIR_APPLICATION', DIR_OPENCART . 'adminpage/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/home/xjgqivxn/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'xjgqivxn_ocar532');
+define('DB_PASSWORD', 'Ge10112013!?');
+define('DB_DATABASE', 'xjgqivxn_ocar532');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
