c71af10b069880a06ac208895d59b731901242ba
Volodymyr Gogunsky <gogunsky.vladimir@gmail.com>
2025-12-12 11:55:57 +0200

    Updated checkout

--- catalog/language/uk-ua/information/information.php
+++ catalog/language/uk-ua/information/information.php
@@ -2,8 +2,13 @@
 // Common Information Page Titles Translations
 // These translations can be used when creating information pages in the admin panel
 
-// Note: Information page titles are stored in the database and can be translated per page.
-// Use these translations when creating/editing information pages:
+// Note: Information page titles are stored in the database (oc_information_description table) 
+// and can be translated per page and per language.
+// 
+// To translate existing pages, run the SQL script:
+// shared/sql/translate_footer_information_pages_ukrainian.sql
+//
+// Use these translations when creating/editing information pages in the admin panel:
 // 
 // Terms & Conditions → Умови використання
 // Delivery Information → Інформація про доставку
