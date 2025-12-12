dfa7f6a9b3d1ac054419fd71a393e9ecc03211ea
Volodymyr Gogunsky <gogunsky.vladimir@gmail.com>
2025-12-12 11:33:19 +0200

    Added localization

---
+++ catalog/language/uk-ua/product/compare.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = 'Порівняння товарів';
+
+// Text
+$_['text_product']      = 'Деталі товару';
+$_['text_name']         = 'Товар';
+$_['text_image']        = 'Зображення';
+$_['text_price']        = 'Ціна';
+$_['text_model']        = 'Модель';
+$_['text_manufacturer'] = 'Бренд';
+$_['text_availability'] = 'Наявність';
+$_['text_rating']       = 'Рейтинг';
+$_['text_reviews']      = 'На основі %s відгуків.';
+$_['text_summary']      = 'Підсумок';
+$_['text_weight']       = 'Вага';
+$_['text_dimension']    = 'Розміри (Д x Ш x В)';
+$_['text_compare']      = 'Порівняння товарів (%s)';
+$_['text_success']      = 'Успіх: Ви додали <a href="%s">%s</a> до <a href="%s">порівняння товарів</a>!';
+$_['text_remove']       = 'Успіх: Ви змінили ваше порівняння товарів!';
+$_['text_no_results']   = 'Ви не обрали жодного товару для порівняння.';
+
+// Error
+$_['error_product']     = 'Попередження: Товар не знайдено!';
+
