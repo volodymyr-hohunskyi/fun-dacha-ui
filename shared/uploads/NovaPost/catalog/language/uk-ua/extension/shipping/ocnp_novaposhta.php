35e2fb956642854edfda0424a8b0736faf37e8c6
Volodymyr Gogunsky <gogunsky.vladimir@gmail.com>
2026-03-28 16:37:09 +0200

    Clean up

--- shared/uploads/NovaPost/catalog/language/uk-ua/extension/shipping/ocnp_novaposhta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$_['text_description'] = 'Доставка Новою Поштою';
-$_['text_ocnp_city_label'] = 'Місто';
-$_['text_ocnp_warehouse_label'] = 'Відділення';
-$_['text_ocnp_area_label'] = 'Область';
-$_['text_ocnp_select_area_placeholder'] = '-- Виберіть область --';
-$_['text_ocnp_select_city_placeholder'] = '-- Виберіть місто --';
-$_['text_ocnp_select_warehouse_placeholder'] = '-- Виберіть відділення --';
